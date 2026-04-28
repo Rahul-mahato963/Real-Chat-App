50088d05949e3bfc28e545f8feccb2de437e0620
Rahul-mahato963 <rahulmahaseth146@gmail.com>
2026-04-28 09:29:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'https://real-chat-app-backend-gqko.onrender.com',
+    origin:'https://real-chat-app-ke0t.onrender.com',
     credentials:true
 };
 app.use(cors(corsOption)); 
